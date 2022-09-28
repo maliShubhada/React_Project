557234c5a0d988b21f2bc23270bac80d449fee07
maliShubhada <shubhada.mali@yash.com>
2022-09-28 21:31:24 +0530

    Done with Remaining project work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Navbar from './components/Navbar';
 import {Routes,Route}from 'react-router-dom'
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 function App() {
   return (
     <>
     <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
+      <Route path='/services' element={<Services/>}></Route>
+      <Route path='/products' element={<Products/>}></Route>
+      <Route path='/sign-up' element={<SignUp/>}></Route>
     </Routes>
     </>
     
